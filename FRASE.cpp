6f34c48c3d4204f34b3a9423d8d348eba0dc216a
lalliantonella <lalliantonella03@gmail.com>
2024-04-21 13:06:31 -0300

    first commit

---
+++ FRASE.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main () {
+string FRASEINCOMPLETA = "MI PAIS ES ";
+FRASEINCOMPLETA.insert (FRASEINCOMPLETA.length (), "ARGENTINA");
+cout << FRASEINCOMPLETA;
+ return 0;
+}
+
